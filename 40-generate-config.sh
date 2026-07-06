e628373113bafb2eb88774e92defaa886e5ffe0b
mckenna654 <mckenna654@gmail.com>
2026-07-06 13:37:26 +1000

    fix: restore native shell parameter expansion in config generator

--- 40-generate-config.sh
+++ 40-generate-config.sh
@@ -4,7 +4,7 @@
 envsubst '$NGINX_PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Generate application config
-envsubst '$DISPATCHARR_URL $DISPATCHARR_API_KEY' <<'JSON_EOF' > /usr/share/nginx/html/config.json
+cat <<JSON_EOF > /usr/share/nginx/html/config.json
 {
   "url": "${DISPATCHARR_URL:-}",
   "apiKey": "${DISPATCHARR_API_KEY:-}"
